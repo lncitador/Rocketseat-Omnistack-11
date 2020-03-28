996f2278e6fc19920341e9f39a6554cd1713a066
Walaff Fernandes <walaffonofre@icloud.com>
2020-03-28 18:42:30 -0300

    Mobile da aplicação

---
+++ mobile/App.js
@@ -0,0 +1,12 @@
+import "intl";
+import "intl/locale-data/jsonp/pt-BR";
+
+import React from "react";
+
+import Routes from "./src/routes";
+
+import "./src/config/statusBarConfig";
+
+export default function App() {
+  return <Routes />;
+}
